533b5333ee74fca23dff590e595c0f43bc01b074
Conor Schaefer <conor.schaefer@gmail.com>
2013-03-02 08:52:24 -0500

    just grab first ZIP file on page

--- update-phone.pl
+++ update-phone.pl
@@ -50,12 +50,10 @@ say "Starting URL is: $start";
 my $mech = WWW::Mechanize->new( autocheck => 1 );
 $mech->get( $start );
 
-my @zips = $mech->find_all_links( url_regex => qr/\d{8}\-\w+\-$device\.zip$/ );
+my $zip = ( $mech->find_all_links( url_regex => qr/\d{8}\-\w+\-$device\.zip$/ ) )[0]; # first link on page is most recent;
 
-my $zip = shift @zips; # take the first argument, because we want the newest;
-my $url = $zip->url_abs; # retrieve full URL from object;
-say "Will now GET this URL: \n\t$url";
-$mech->get( $url ); # go grab that url;
+my $url = $zip->url_abs;    # retrieve full URL from object;
+say "Fetching image at $url" if $verbose;    # chatty output;
+$mech->get( $url );                          # go grab that url;
 $mech->save_content( 'cyanogenmod-image.zip' ) or die "Could not save image to disk $!";
-
 
