ae69ada45188c649b2750327340d51c3fbe40d06
Conor Schaefer <conor.schaefer@gmail.com>
2013-03-02 07:38:38 -0500

    initial commit, working grab of zipped image

---
+++ update-phone.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+# quick script to grab newest video file from phone;
+use strict;
+use warnings;
+use diagnostics;       # useful for debugging;
+use feature 'say';     # beats print;
+use Getopt::Long;      # for parsing command-line options;
+use WWW::Mechanize;    # for reading web pages programmatically;
+use Storable;          # for saving files found by WWW::Mechanize;
+
+my $usage = <<'END';
+ScriptName
+
+. 
+
+Usage: 
+     --option 1        
+     --option 2    
+     --help        # show this usage information
+
+Supported options:
+
+
+    -b, --build         # 
+    -d, --device         # specify device for cyanogen mod
+    -h, --help         # show this usage information
+    -v, --verbose        # enable chatty output
+
+END
+
+GetOptions(
+    'device|d'       => \my $device,
+    'build|b'        => \my $build,
+    'type|t'         => \my $type,
+    'help|h|?|usage' => \my $help,
+    'verbose|v'      => \my $verbose,
+) or die "$usage";
+
+if ( $help ) {    # if user requested usage information;
+    say $usage;    # display usage information;
+    exit 0;        # exit cleanly;
+}
+
+$device = 'maguro'  unless $device;
+$type   = 'nightly' unless $type;
+
+my $start = "http://download.cyanogenmod.com/?device=$device&type=$type";
+say "Starting URL is: $start";
+
+my $mech = WWW::Mechanize->new( autocheck => 1 );
+$mech->get( $start );
+
+my @images = $mech->find_all_links( url_regex => qr/\d{8}\-\w+\-$device\.zip$/ );
+say "These are all the links we found:";
+#say "\t$_->url_abs" for @images;
+
+
+for my $image ( @images ) {
+    my $url      = $image->url_abs;
+    say "Will not GET this URL: \n\t$url";
+    $mech->get( $url );
+    $mech->save_content( 'cyanogenmod-image.zip' ) or die "Could not save image to disk $!";
+    last;
+    
+    my $filename = $url;
+    say $url;
+    $filename =~ s[^.+/][];
+    store( $url, 'cyanogenmod-image.zip' ) or die "Could not save image to disk $!";
+
+#    print "Fetching $url";
+#    $mech->get( $url, ':content_file' => $filename );
+#
+#    print "   ", -s $filename, " bytes\n";
+}
